16340c226c93f4605e10a4e047de016db61fca10
eferreira <eferreira@roshka.com>
2015-07-16 09:33:53 -0400

    Initial Commit

---
+++ PruebaSlideMenuWithLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaSlideMenuWithLibrary
+//
+//  Created by Eliana Ferreira on 7/16/15.
+//  Copyright (c) 2015 roshka.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
